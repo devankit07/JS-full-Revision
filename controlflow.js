1ae6ca8f5e1a8624ab05d0e88aa089b36dd45e9f
Ankitkumar277 <Ankitrathor272005@gmail.com>
2025-11-01 17:08:23 +0530

    intermediate topic

---
+++ controlflow.js
@@ -0,0 +1,60 @@
+// control flow ka js mai use hota hai kuch krane ke liye hum option dete hai agar ye condition match kre uska code run ho jaye 
+
+
+// truthy falsy :- agar condition ki value 0 "" null undefiened false document.all inme se kuch bhi hui to bo false . baki sab ture
+
+// let value = 0; // Try changing to "", null, undefined, NaN, etc.
+
+// if (value) {
+//   console.log(" Truthy value");
+// } else {
+//   console.log(" Falsy value");
+// }
+
+
+
+// if else :- pehli conditon true to if ka code brna else
+
+    // let a = 14;
+    // if(a<= 0){
+    //     console.log("sahi pkde hai");
+    // }else{
+    //     console.log("galat pkde hai");
+    // };
+
+// if else if else :- 2-3 option jyada brna else
+
+// let number = 0;
+// if (number > 0) {
+//   console.log("Positive number");
+// } else if (number < 0) {
+//   console.log("Negative number");
+// } else {
+//   console.log("Number is zero");
+// }
+
+//switch case :- jo case switch se match hoga bo code run hoga brna default ka code, case mai break lgane se agr uska case match hua to wahi ruk jaega ange check mhi hoga 
+
+
+// let day = 3;
+
+// switch (day) {
+//   case 1:
+//     dayName = "Monday";
+//     break;
+//   case 2:
+//     dayName = "Tuesday";
+//     break;
+//   case 3:
+//     dayName = "Wednesday";
+//     break;
+//   default:
+//     dayName = "Invalid day number";
+// }
+
+// console.log(dayName);
+
+
+
+    
+
