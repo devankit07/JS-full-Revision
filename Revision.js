fe21246b4911f0e286ac716aacce10307ef34e5a
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-02-03 23:42:23 +0530

    variables

--- Revision.js
+++ Revision.js
@@ -21,23 +21,72 @@
 //scope in javascript
 
 // global scope -> pure code mai kahi bhi access kar sakte hai
-var a = 45; // global scope
+// var a = 45; // global scope
 
-function func1() {
-  console.log(a);
-}
-func1();
-// function scope -> kisi function ke andar jo variable banaya jata hai wo sirf usi function ke andar access hota hai  
-function func2() {
-  var b = 56;
-  console.log(b);
-}
-func2(); 
-// block scope-> jo variable kisi block ke andar banaya jata hai wo sirf usi block ke andar access hota hai
-{
-  let c = 67;
-  const d = 78;
-  console.log(c);
-  console.log(d);
-}
+// function func1() {
+//   console.log(a);
+// }
+// func1();
+// // function scope -> kisi function ke andar jo variable banaya jata hai wo sirf usi function ke andar access hota hai  
+// function func2() {
+//   var b = 56;
+//   console.log(b);
+// }
+// func2(); 
+// // block scope-> jo variable kisi block ke andar banaya jata hai wo sirf usi block ke andar access hota hai
+// {
+//   let c = 67;
+//   const d = 78;
+//   console.log(c);
+//   console.log(d);
+// }
+
+// hoisting and tdz(temporal dead zone)
+
+
+
+
+
+// 1 variables 
+//var (function scope , reassign , redeclare , hoisting -> undefned & old )
+
+// function abc(){
+//   var x = 14;
+//   x=15;//reassign
+//   var x=16//redeclare
+//   console.log(x);//fnc scope
+// }
+// abc()
+
+// console.log(f);
+// var f=7 // undefined 
+
+
+
+//let (block scope, reassign, redeclare nahi kr skte , hoisting → error & new )
+
+// {//block scope
+//   let x =14;
+//   x =15;// reassign 
+//   // let x =16 //redeclare nahi hoga
+//   console.log(x);
+// }
+
+// console.log(c);
+// let c =1 // error 
+
+
+
+//const(block scope, reassign & redeclare kuch bhi nahi , hoisting-> error & value fix + new )
+
+// {
+//   const a =14;
+//   // a =15; not reassign
+//   // const a =16 not redeclare
+//   console.log(a);
+// }
+
+
+// console.log(c);
+// const c = 11;  error
 
